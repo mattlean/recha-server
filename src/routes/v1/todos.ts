99f89868197e082d0b4ab5e49129824e5b9c42da
Matthew Lean <matt@mattlean.com>
2019-02-08 03:57:29 -0800

    Refactor todo endpoints to use end middleware

--- src/routes/v1/todos.ts
+++ src/routes/v1/todos.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import { createTodo, deleteTodo, getTodoById, getTodos, patchTodo } from '../../util/db/todos'
 import { formatAPIRes } from '../../util'
+import { genErr } from '../../util/err'
 import { db } from '../../app'
 import { TYPE } from '../../types/Todo'
 
@@ -10,34 +11,58 @@ const router = Router()
 router.get('/', (req, res, next) => {
   const { col, date, dir } = req.query
   return getTodos(db, date, col, dir)
-    .then(result => res.json(formatAPIRes(result, TYPE)))
+    .then(result => {
+      res.locals.result = result
+      return next()
+    })
     .catch(err => next(err))
 })
 
 router.get('/:id', (req, res, next) =>
   getTodoById(db, req.params.id)
-    .then(result => res.json(formatAPIRes(result, TYPE)))
+    .then(result => {
+      res.locals.result = result
+      return next()
+    })
     .catch(err => next(err))
 )
 
 router.post('/', (req, res, next) => {
   const { completed_at, date, details, name, order_num } = req.body
   return createTodo(db, { completed_at, date, details, name, order_num })
-    .then(result => res.status(201).json(formatAPIRes(result, TYPE)))
+    .then(result => {
+      res.status(201)
+      res.locals.result = result
+      return next()
+    })
     .catch(err => next(err))
 })
 
 router.patch('/:id', (req, res, next) => {
   const { completed_at, date, details, name, order_num } = req.body
   return patchTodo(db, req.params.id, { completed_at, date, details, name, order_num })
-    .then(result => res.json(formatAPIRes(result, TYPE)))
+    .then(result => {
+      res.locals.result = result
+      return next()
+    })
     .catch(err => next(err))
 })
 
 router.delete('/:id', (req, res, next) =>
   deleteTodo(db, req.params.id)
-    .then(result => res.json(formatAPIRes(result, TYPE)))
+    .then(result => {
+      res.locals.result = result
+      return next()
+    })
     .catch(err => next(err))
 )
+
+router.use((req, res, next) => {
+  const allowedMethods = ['GET', 'DELETE', 'PATCH', 'POST']
+  if (allowedMethods.indexOf(req.method) > -1 && res.locals.result) {
+    return res.json(formatAPIRes(res.locals.result, TYPE))
+  }
+  return next(genErr(404))
+})
 
 export default router
