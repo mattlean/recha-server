fe6063396f87626c692e4f76bfa08172533b70b5
Matthew Lean <matt@mattlean.com>
2020-10-20 23:17:51 -0700

    Setup TS for frontend

--- webpack/config.js
+++ webpack/config.js
@@ -7,21 +7,23 @@ const merge = require('webpack-merge')
 module.exports = () => {
   return merge([
     {
-      entry: `${FRONT.SRC}/index.jsx`,
+      entry: `${FRONT.SRC}/index.tsx`,
       output: {
         path: FRONT.BUILD,
       },
       resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
 
     setMode('development'),
 
     compileJS({
+      test: /\.(j|t)sx?$/i,
       include: [FRONT.SRC],
       options: {
         presets: [
+          '@babel/preset-typescript',
           ['@babel/preset-react', { development: true, runtime: 'automatic' }],
         ],
       },
